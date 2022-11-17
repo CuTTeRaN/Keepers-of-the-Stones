9ba81f08bf5fd8f936e83adb0cc0e8ada573c020
MagicalAlexey <69478092+MagicalAlexey@users.noreply.github.com>
2022-11-17 14:54:41 +0300

    Little changes for level_set argument

--- src/main/java/power/keepeersofthestones/procedures/LevelSetProcedure.java
+++ src/main/java/power/keepeersofthestones/procedures/LevelSetProcedure.java
@@ -30,9 +30,6 @@ public class LevelSetProcedure {
 			e.printStackTrace();
 		}
 		if (entity instanceof Player _player && !_player.level.isClientSide())
-			_player.displayClientMessage(Component.literal(
-					("The player has successfully set the level: " + (entity.getCapability(PowerModVariables.PLAYER_VARIABLES_CAPABILITY, null)
-							.orElse(new PowerModVariables.PlayerVariables())).power_level)),
-					(false));
+			_player.displayClientMessage(Component.literal("The player has successfully set the level!"), (false));
 	}
 }
